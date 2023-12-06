3f2de1eb159fcb01b72929ccf2629b0896a6c465
Johannes Drescher <jdrescher@smartlytic.de>
2023-12-06 20:21:36 +0100

    day01 solved

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -3,20 +3,31 @@ package day01
 import println
 import readInput
 
+val words = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val wordsReplace = listOf("o1e", "t2o", "th3ee", "fo4r", "fi5e", "s6x", "se7en", "ei8ht", "n9ne")
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day01/Day01_test1")
+    val input = readInput("day01/Day01")
 
-    val input = readInput("Day01")
+    check(part1(testInput) == 142)
     part1(input).println()
+
+    check(part2(testInput) == 281)
     part2(input).println()
+}
+
+fun part1(input: List<String>): Int {
+    return input.sumOf { line ->
+        line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+    }
+}
+
+fun part2(input: List<String>): Int {
+    return input.sumOf { line ->
+        var tmpLine = line
+        words.forEachIndexed { index, word -> tmpLine = tmpLine.replace(word, wordsReplace.elementAt(index)) }
+        tmpLine.first { it.isDigit() }.digitToInt() * 10 + tmpLine.last { it.isDigit() }.digitToInt()
+    }
 }
